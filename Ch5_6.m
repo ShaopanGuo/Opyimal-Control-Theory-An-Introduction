f1bc2570af02b05f07d0cd9509b6014c5e0a3637
ShaopanGuo <81441834+ShaopanGuo@users.noreply.github.com>
2021-04-30 18:11:19 -0500

    Create Ch5_6.m

---
+++ Ch5_6.m
@@ -0,0 +1,18 @@
+% 5-6
+% EE7560 Homework #3 Q2
+
+syms s t x0 tao phi phi0;
+A=[-1 -1;-3 1];
+I=[1 0;0 1];
+E=s*I-A;
+C=det(E);
+D=collect(inv(E));
+F=ilaplace(D);
+% pretty(F)
+
+syms x
+phi = subs(F,t,T-t);
+lambda = phi(2,1)/phi(2,2)*x;
+% pretty(lambda)
+u = -lambda
+pretty(u)
